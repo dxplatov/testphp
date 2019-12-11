d9819748942d5cd1e44b2921d814aa827a3b0e79
Doston <doston@Dostons-MacBook-Pro.local>
2019-12-11 20:22:01 +0500

    goo

--- getfilecontent.php
+++ getfilecontent.php
@@ -4,8 +4,4 @@
     //header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: GET');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-    $name = $_GET['name'];
-    $phone = $_GET['phone'];
-    file_put_contents("test.txt",$name);
-    file_put_contents("test.txt",$phone);
-   
+    echo file_get_contents("./test.txt");
